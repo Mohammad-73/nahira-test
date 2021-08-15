15c39f045967b877e5a05aa90633569a349a5952
Mohammad-73 <m.khalili7310@gmail.com>
2021-08-15 15:54:42 +0430

    import login component to sitelayout

--- src/components/layout/SiteLayout.jsx
+++ src/components/layout/SiteLayout.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import Login from "../login/Login";
 import classes from "./SiteLayout.module.scss";
 
 export default class SiteLayout extends Component {
   render() {
     return (
       <div className={classes.root}>
-        <div className={classes.siteLayout}>hello</div>
+        <div className={classes.siteLayout}>
+          <Login />
+        </div>
       </div>
     );
   }
