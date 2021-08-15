057193e96356d0e5cb1e7ad1013a3ea69419e779
Mohammad-73 <m.khalili7310@gmail.com>
2021-08-15 20:07:08 +0430

    style to Login component

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import classes from "./Login.module.scss";
 import { useSelector } from "react-redux";
+import emailValidationRegex from "../../helper/emailValidationRegex";
 import userActions from "../../store/user/user.actions";
 
 export default function Login() {
   const user = useSelector((state) => state.user);
+  const [emailValidation, setEmailValidation] = useState(true);
   const [loginValue, setLoginValue] = useState({
     userEmail: "",
     userPass: "",
@@ -21,29 +23,52 @@ export default function Login() {
   function handleSubmit(e) {
     e.preventDefault();
   }
+  useEffect(() => {
+    setEmailValidation(emailValidationRegex(userEmail));
+  }, [userEmail]);
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h2>{user}</h2>
-        <input
-          value={userEmail}
-          name="userEmail"
-          onChange={handleChange}
-          type="email"
-        />
-        <input
-          value={userPass}
-          name="userPass"
-          onChange={handleChange}
-          type="password"
-        />
-        {loginValue.userEmail}
-        <button onClick={() => userActions.login(userEmail, userPass)}>
-          login
-        </button>
-        <button onClick={() => userActions.logout()}>logout</button>
-      </form>
+    <div className={classes.rootLogin}>
+      <div className={classes.loginForm}>
+        <form onSubmit={handleSubmit}>
+          <div className={classes.container}>
+            <label htmlFor="userEmail" />
+            <b>Username</b>
+            <input
+              value={userEmail}
+              name="userEmail"
+              onChange={handleChange}
+              type="email"
+              placeholder="Enter Email"
+              required
+            />
+            <div
+              className={
+                emailValidation ? `${classes.valid}` : `${classes.invalid}`
+              }
+            >
+              <span>{emailValidation ? "valid" : "invalid"}</span>
+            </div>
+            <label htmlFor="userPass" />
+            <b>Password</b>
+            <input
+              value={userPass}
+              name="userPass"
+              onChange={handleChange}
+              type="password"
+              placeholder="Enter Password"
+              required
+            />
+            <button
+              onClick={() => userActions.login(userEmail, userPass)}
+              type="submit"
+            >
+              Login
+            </button>
+            <p>{user}</p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
