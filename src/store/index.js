ba9b118871e10bad20fc2f20fc1242b010cdb667
Mohammad-73 <m.khalili7310@gmail.com>
2021-08-15 15:53:35 +0430

    install packages

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
 import userReducer from "./user/user.reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const reducer = combineReducers({
+const reducers = combineReducers({
   user: userReducer,
 });
 
-const store = createStore(reducer);
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(ReduxThunk))
+);
 
 export default store;
