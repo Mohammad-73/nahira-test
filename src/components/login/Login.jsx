26ce02b63e92d634169c93c4f06c8ac67b7483ca
Mohammad-73 <m.khalili7310@gmail.com>
2021-08-15 16:38:08 +0430

    change login component to controlled component

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import userActions from "../../store/user/user.actions";
 
 export default function Login() {
   const user = useSelector((state) => state.user);
+  const [loginValue, setLoginValue] = useState({
+    userEmail: "",
+    userPass: "",
+  });
+
+  function handleChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+    setLoginValue({ ...loginValue, [name]: value });
+  }
 
   return (
     <div>
-      <h2>{user}</h2>
-      <button onClick={() => userActions.login()}>login</button>
-      <button onClick={() => userActions.logout()}>logout</button>
+      <form>
+        <h2>{user}</h2>
+        <input
+          value={loginValue.userEmail}
+          name="userEmail"
+          onChange={handleChange}
+          type="email"
+        />
+        <input
+          value={loginValue.userPass}
+          name="userPass"
+          onChange={handleChange}
+          type="password"
+        />
+        {loginValue.userEmail}
+        <button onClick={() => userActions.login()}>login</button>
+        <button onClick={() => userActions.logout()}>logout</button>
+      </form>
     </div>
   );
 }
